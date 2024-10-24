8b191505a26e4056bdbf3ca0532d1992a37d613b
KarlDragon <giocodeproject@gmail.com>
2024-10-24 20:13:13 +0700

    I think this is the best thing I can do for this task

--- DE_NGHE_AN_24_25_LOP12/BIEUTHUCNT.cpp
+++ DE_NGHE_AN_24_25_LOP12/BIEUTHUCNT.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 void IO(){
 	freopen("BIEUTHUCNT.INP","r",stdin);
     freopen("BIEUTHUCNT.OUT","w",stdout);
@@ -7,6 +8,7 @@ void IO(){
     cin.tie(0);
     cout.tie(0);
 }
+
 vector<bool> is_prime(int n) {
     vector<bool> prime(n + 1, true);
     prime[0] = prime[1] = false; 
@@ -21,54 +23,21 @@ vector<bool> is_prime(int n) {
     return prime;
 }
 
-int sol_1(int n, vector<bool> prime_arr){
-	int count = 0;
+int findPair(int n, vector<bool> prime_arr){
+	int count = 0,y;
 	
-	//Brute force
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (prime_arr[(i + 2 * j)]) {
-//                cout << i << " " << j << endl;
-                count++;
-            }
-        }
-    }
-    cout<<count;
+	for (int p=3; p<=3*n; p+=2){
+		if (prime_arr[p]){
+			for (int x = 1; x<=n; x+=2){
+				y = p - x;
+				if (y >= 1 && y/2 <= n){
+					count++;
+				}
+			}
+		}
+	}
+	cout<<count;
 }
-int sol_2(int n, vector<bool> prime_arr){
-	int count = 0;
-	
-	    for (int y = 1; y <= n; y++) {
-        // We want to check x + 2y, so we compute p = x + 2y and x = p - 2y
-        // p = 2*y + 1 or p = 2*y + x with x = 1 and keep x++ until the loop is end
-        for (int p = 2 * y + 1; p <= 3 * n; p++) {
-            if (prime_arr[p]) {
-                int x = p - 2 * y;
-                if (x >= 1 && x <= n) {
-                    count++;
-                }
-            }
-        }
-    }
-    cout<<count;
-}
-int sol_3(int n, vector<bool> prime_arr){
-	int count = 0;
-	
-	     for (int p = 2; p <= 3 * n; p++) {
-        if (prime_arr[p]) {
-            // For each prime, we find all possible (x, y) such that p = x + 2y
-            for (int y = 1; y <= n; y++) {
-                int x = p - 2 * y;
-                if (x >= 1 && x <= n) {
-                    count++;
-                }
-            }
-        }
-    }
-    cout<<count;
-}
-
 int main() {
 	IO();
 	
@@ -77,7 +46,7 @@ int main() {
     cin >> n;
 
 	prime_arr = is_prime(3*n);
-    sol_3(n,prime_arr);
+    findPair(n,prime_arr);
 
     return 0;
 }
